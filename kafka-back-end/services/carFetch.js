bbfc34d7042092c37c6bf294db534602caf6d932
Pooja-SD <32438760+Pooja-SD@users.noreply.github.com>
2017-11-28 13:26:33 -0800

    admin can fetch cars using this api

---
+++ kafka-back-end/services/carFetch.js
@@ -0,0 +1,39 @@
+var mysql = require("./mysql");
+function handle_request(msg, callback) {
+
+    var res = {};
+    console.log("In handle request:" + JSON.stringify(msg));
+
+    var getUser = "select * from carDemo where carID = '" + msg.carID + "' or carName = '" + msg.carName + "'";
+    console.log("Query is:" + getUser);
+
+    mysql.fetchData(function (err, results) {
+        if (err) {
+            throw err;
+        }
+
+        else {
+            if (results.length > 0) {
+                console.log(results);
+                res.value = "200";
+                res.message = results;
+                console.log(res);
+
+
+            }
+            else {
+
+                console.log("no flights fetched with the given ID");
+                res.value = "404";
+                res.message = "No flight exists with the given ID ";
+
+            }
+            console.log("inside try:" + res);
+            callback(null, res);
+        }
+
+    }, getUser);
+
+}
+
+exports.handle_request = handle_request;
