fa6ecc82407e18ed7801e11f8384ffbefdfbfcd9
Pooja-SD <32438760+Pooja-SD@users.noreply.github.com>
2017-11-29 14:30:03 -0800

    update server.js
    include admin/dashboard service

--- kafka-back-end/server.js
+++ kafka-back-end/server.js
@@ -628,3 +628,24 @@ consumer_payFlight.on('message', function (message) {
         return;
     });
 });
+consumer_top5LocationsForCars.on('message', function (message) {
+    console.log('message received');
+    console.log(JSON.stringify(message.value));
+    var data = JSON.parse(message.value);
+    top5LocationsForCars.handle_request(data.data, function(err,res){
+        console.log('after handle'+res);
+        var payloads = [
+            { topic: data.replyTo,
+                messages:JSON.stringify({
+                    correlationId:data.correlationId,
+                    data : res
+                }),
+                partition : 0
+            }
+        ];
+        producer.send(payloads, function(err, data){
+            console.log(data);
+        });
+
+        return;
+    });
