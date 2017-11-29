45fde218841b4c0d28ea3fe6f94b4e9d9f8eda7a
Pooja-SD <32438760+Pooja-SD@users.noreply.github.com>
2017-11-29 14:42:29 -0800

    Update bookFlight.js

--- kafka-back-end/services/bookFlight.js
+++ kafka-back-end/services/bookFlight.js
@@ -1,6 +1,29 @@
 var mysql = require("./mysql");
 function handle_request(msg, callback) {
+   var flightBookServiceCount;
+    var service="flightBook";
+    var getUser="select count from servicesCount where service='"+service+"'";
+    console.log("Query:"+getUser);
+    mysql.fetchData(function(err,results){
 
+        console.log("flight Service count:"+JSON.stringify(results));
+
+        console.log("flight service count:"+JSON.stringify(results[0].count));
+        flightBookServiceCount=+JSON.stringify(results[0].count);
+
+
+
+        flightBookServiceCount++;
+        console.log("carServiceCount:"+flightBookServiceCount);
+
+        var getUser="update servicesCount set count='"+flightBookServiceCount+"' where service='"+service+"'";
+        console.log("Query is:"+getUser);
+        mysql.fetchData(function(err,results){
+            if(err) throw err;
+            console.log(results.affectedRows + "records updated");
+
+        },getUser);
+    },getUser);
     var res = {};
     console.log("In handle request flights:" + JSON.stringify(msg)); 
     console.log(msg);
