da90dc1751948f91fb2613ecb875b0095aece428
Pooja-SD <32438760+Pooja-SD@users.noreply.github.com>
2017-12-03 13:24:59 -0800

    Update bill.js

--- kafka-back-end/services/bill.js
+++ kafka-back-end/services/bill.js
@@ -34,6 +34,66 @@ function handle_request(msg, callback) {
             }
 
         }, getUser);
+    }else if(bookingType="hotel"){
+        var getUser = "select * from transactionHotels where bookID='"+msg.billID+"'";
+        console.log("Query is:" + getUser);
+
+        mysql.fetchData(function (err, results) {
+            if (err) {
+                throw err;
+            }
+
+            else {
+                if (results.length > 0) {
+                    console.log(results);
+                    res.value = "200";
+                    res.message = results;
+                    console.log(res);
+
+
+                }
+                else {
+
+                    console.log("no bills exists with the given ID");
+                    res.value = "404";
+                    res.message = "No bills exists with the given ID ";
+
+                }
+                console.log("inside try:" + res);
+                callback(null, res);
+            }
+
+        }, getUser);
+    }else{
+        var getUser = "select * from transactionFlights where bookID='"+msg.billID+"'";
+        console.log("Query is:" + getUser);
+
+        mysql.fetchData(function (err, results) {
+            if (err) {
+                throw err;
+            }
+
+            else {
+                if (results.length > 0) {
+                    console.log(results);
+                    res.value = "200";
+                    res.message = results;
+                    console.log(res);
+
+
+                }
+                else {
+
+                    console.log("no bills exists with the given ID");
+                    res.value = "404";
+                    res.message = "No bills exists with the given ID ";
+
+                }
+                console.log("inside try:" + res);
+                callback(null, res);
+            }
+
+        }, getUser);
     }
 
 
